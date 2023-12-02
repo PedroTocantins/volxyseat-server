defe70776fb5a9f26e987237ed331b77df280669
PedroTocantins <pedrotocantins11s@gmail.com>
2023-12-02 15:30:11 -0300

    transaction commit

---
+++ src/Volxyseat.Infrastructure/Migrations/20231202161511_issueDateMigration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Volxyseat.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class issueDateMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IssueDate",
+                table: "Subscriptions");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "IssueDate",
+                table: "Subscriptions",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
